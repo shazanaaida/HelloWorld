f2399bf1d461dcfd874a6c1dfca8a8ae8b303088
shazanaaida <47972859+shazanaaida@users.noreply.github.com>
2019-02-26 00:40:16 +0800

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Hai my name is Nur Aida Shazana Ahmad - 1615504</Text>
-      <Text>Hai Im the second studen : Puteri Nur Liyana Nasaruddin 1610012</Text>
+      <Text>Hai Im the second student : Puteri Nur Liyana Nasaruddin 1610012</Text>
 
       </View>
     );
